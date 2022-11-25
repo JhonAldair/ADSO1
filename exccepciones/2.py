aac42747d4d7ea85c18bbdcd4eb9a247209f3df2
JhonAldair <jhonalda14@gmail.com>
2022-11-24 21:18:15 -0500

    excepcion

--- exccepciones/2.py
+++ exccepciones/2.py
@@ -6,14 +6,14 @@ while True:
         if z=='suma' or z=='resta':
             a=float(input('ingrese primer numero: '))
             b=float(input('ingrese segundo numero: '))
-            c=float(input('ingrese tercer numero: '))
+            c=float(input('ingrese tercer numero; '))
             suma=-b+math.sqrt(b**2-4*a*c)/(2*a)
             resta=-b-math.sqrt(b**2-4*a*c)/(2*a)
             if z=='suma':
                 print(suma)
             if z=='resta':
                 print(resta)
-        else:
+        else: 
             print('eliga una de las dos opciones')
     except ZeroDivisionError:
         print('no se puede dividir entre cero')
