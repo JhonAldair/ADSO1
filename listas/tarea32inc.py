1010b4f45ea9919ad72ef737c8fb0d0461b0b842
JhonAldair <116193336+JhonAldair@users.noreply.github.com>
2022-12-14 08:22:22 -0500

    Update tarea32inc.py

--- listas/tarea32inc.py
+++ listas/tarea32inc.py
@@ -1,19 +1,20 @@
+'''codigo hecho por:
+Nicolas Juez'''
 """Llenar una lista de tamaño aleatorio entre 10 y 25 elementos. Llene la lista con números 
 aleatorios. Muestre cuales y cuantos son primos"""
-def divi():
-    num1=vector
-    i=1
-    while(num1>=i):
-        if num1%i==0:
-            print(i)
-        i+=1
+
 import random
-tam=random.randint(10,25)
-vector = [round(random.random()*100) for _ in range(tam)]
-print(vector.__len__())
-a=[]
-for i in range(tam):
-    if divi()==2:
-        a.append(vector[i])
-print('la cantidad total de pares fue ',len(a))
-print('la suma de los pares es ',a)
+vector=[]
+rango=random.randint(10,25)
+for n in range (rango):
+    vector.append(round(random.random()*100))
+print('lista:',vector)
+for n in vector:
+    i=1
+    cont=0
+    while(n>=i):
+        if n%i==0:
+            cont+=1
+        i+=1
+    if not cont > 2 or n <=1:
+        print('el numero',n,'es primo')
