9acab06a68e3ecb7a33d8b3ed30a7540c885a3e1
JhonAldair <116193336+JhonAldair@users.noreply.github.com>
2022-12-14 08:25:54 -0500

    Update tarea17.py

--- condicionales/tarea17.py
+++ condicionales/tarea17.py
@@ -1,4 +1,6 @@
-'''7. Un obrero necesita calcular su salario semanal, el cual se obtiene de la sig. manera:
+'''este codigo esta hecho por:
+Juan Camilo Lopez
+7. Un obrero necesita calcular su salario semanal, el cual se obtiene de la sig. manera:
 Si trabaja 40 horas o menos se le paga $2600 por hora
 Si trabaja más de 40 horas se le paga $2600 por cada una de las primeras 40 horas y $5000 por cada hora extra'''
 
