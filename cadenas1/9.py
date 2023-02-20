b8672845571c9e804eb7d57b8b30afb6d86d63eb
JhonAldair <116193336+JhonAldair@users.noreply.github.com>
2023-02-20 09:09:24 -0500

    Update 9.py

--- cadenas1/9.py
+++ cadenas1/9.py
@@ -1,8 +1,9 @@
 #Invente un cifrado de texto tipo murcielago o César. Puede utilizar alguna formula matemática para este fin.
 def codigo():
     a=input('ingrese palabra:')
+    b=int(input('cuantos numeros quiere sumar al cifrado:'))
     for i in a:
         w=(ord(i))
-        w=w+10
+        w=w+b
         print(chr(w),end='')
 codigo()
